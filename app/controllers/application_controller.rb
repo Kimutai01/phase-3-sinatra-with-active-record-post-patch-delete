9ea155fb5b1bce36ca88447938b41e5127826d4d
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-11-04 17:54:47 +0300

    Completed assignments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,48 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  get '/reviews' do
+    reviews = Review.all.order(:score).limit(10)
+    reviews.to_json(include: {
+      user: { only: [:name] },
+      game: { only: [:title] }
+    })
+
+  end
+
+  get '/reviews/:id' do
+    review = Review.find(params[:id])
+
+    review.to_json(include: {
+      user: { only: [:name] },
+      game: { only: [:title] }
+    })
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      comment: params[:comment],
+      score: params[:score],
+      user_id: params[:user_id],
+      game_id: params[:game_id]
+    )
+
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
 end
